e71eeb8f2948871644bb062e08f629552771d475
tylerlenick <tylerlenick@gmail.com>
2018-10-19 21:19:21 -0400

    3rd build

---
+++ client/src/utils/API.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default {
+  // Gets all players
+  getPlayers: function() {
+    return axios.get("/api/players");
+  },
+  // Deletes the player with the given id
+  deletePlayer: function(id) {
+    return axios.delete("/api/players/" + id);
+  },
+  // Saves a player to the database
+  savePlayer: function(playerData) {
+    return axios.post("/api/players", playerData);
+  },
+
+  getFantasyPros: function(players) {
+    return axios.post("/api/scraper/", players);
+  },
+
+  getAllArticles: function() {
+    return axios.get("/api/scraper");
+  }
+}
+
+
